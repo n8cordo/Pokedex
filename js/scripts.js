901ccb85da05939853dd3c600e5a96000f331fa7
n8cordo <nate.cordova13@gmail.com>
2024-01-27 22:02:25 -0500

    Start of Pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello World');
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass', 'poison'],
+        evolution: 'Ivysaur',
+    },
 
-let favoriteFood = 'enchiladas';
-document.write(favoriteFood);
+    {
+        name: 'Charmander',
+        height: 0.6,
+        type: 'fire',
+        evolution: 'Charmeleon'
+    },
+
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        type: 'water',
+        evolution: 'Wartortle'
+    },
+
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: 'electric',
+        evolution: 'Raichu'
+    },
+
+    {
+        name: 'Cubone',
+        height: 0.4,
+        type: 'ground',
+        evolution: 'Marowak'
+    }
+]
